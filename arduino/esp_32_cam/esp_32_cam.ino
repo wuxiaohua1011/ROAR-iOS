c949f7f88598d894f412a01d8b0edbcb1c66e9fd
Michael Wu <wuxiaohua1011@berkeley.edu>
2021-10-08 20:54:09 -0700

    hmm, it seems like we have a problem with servo's channel

--- arduino/esp_32_cam/esp_32_cam.ino
+++ arduino/esp_32_cam/esp_32_cam.ino
@@ -1,249 +1,204 @@
-/**
- * Dependency:
- *    https://www.arduino.cc/reference/en/libraries/esp32servo/
- * 
- * Get Started:
- * 1. Install ESP 32 Board (Tools -> Board -> Boards Manager, search for ESP32)
- * 2. Select AI Thinker ESP32-Cam in the Tools -> Board -> ESP32 Arduino -> AI Thinker ESP32-Cam
- * 3. Replace WIFI_SSID and WIFI_PASS with your Wifi credentials
- * 4. Click the upload button
- */
-
-#include <esp32cam.h>
 #include <WebServer.h>
 #include <WiFi.h>
-#include <ESP32Servo.h> 
+#include <esp32cam.h>
+#include "esp_camera.h"
+#include <uri/UriBraces.h>
+#include <ESP32Servo.h>
 
 
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
 
-//#include <BLEDevice.h>
-//#include <BLEUtils.h>
-//#include <BLEServer.h>
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+  
 
-//#include <BLE2902.h>
-#include <Arduino.h>
+#define THROTTLE_PIN 14
+#define STEERING_PIN 2
+#define FLASH_LED_PIN 4
+#define RED_LED_PIN 33
 
-#define SERVICE_UUID "ab0828b1-198e-4351-b779-901fa0e0371e"
-#define CONTROL_UUID "19B10011-E8F2-537E-4F6C-D104768A1214"
 
-#define DEVINFO_UUID (uint16_t)0x180a
-#define DEVINFO_MANUFACTURER_UUID (uint16_t)0x2a29
-#define DEVINFO_NAME_UUID (uint16_t)0x2a24
-#define DEVINFO_SERIAL_UUID (uint16_t)0x2a25
-
-#define DEVICE_MANUFACTURER "Intelligent Racing"
-#define DEVICE_NAME "IR Vehicle Control"
-
-bool is_bluetooth_connected = false;
-
-const char HANDSHAKE_START = '(';
-char* WIFI_SSID = "NETGEAR78";
-char* WIFI_PASS = "wuxiaohua1011";
-
-WebServer server(80);
-AsyncWebSocket ws("/test");
-
-//Servo throttleServo;
-//Servo steeringServo;
+// CHANGE YOUR WIFI CREDENTIALS!
+const char* WIFI_SSID = "NETGEAR05";
+const char* WIFI_PASS = "wuxiaohua1011";
 
 static auto loRes = esp32cam::Resolution::find(320, 240);
-static auto hiRes = esp32cam::Resolution::find(800, 600);
-
-volatile int32_t bluetooth_throttle_read = 1500;
-volatile int32_t bluetooth_steering_read = 1500;
 
 
+WebServer server(80);
+Servo throttleServo;
+Servo steeringServo;
+volatile int32_t ws_throttle_read = 1500;
+volatile int32_t ws_steering_read = 1500;
 
-//BLECharacteristic *controlBLECharacteristicMessage;
-//class MyServerCallbacks : public BLEServerCallbacks
-//{
-//    void onConnect(BLEServer *server)
-//    {
-//        Serial.println("Connected");
-//        is_bluetooth_connected = true;
-//    };
-//
-//    void onDisconnect(BLEServer *server)
-//    {
-//        Serial.println("Disconnected");
-//        is_bluetooth_connected = false;
-//    }
-//};
-//
-//
-//class ControlCallback : public BLECharacteristicCallbacks
-//{
-//    void onWrite(BLECharacteristic *characteristic)
-//    {
-//        std::string data = characteristic->getValue();
-//        int n = data.length();
-//        char buf[n+1];
-//        strcpy(buf, data.c_str());
-//        char *token = strtok(buf, ",");
-//        if (token[0] == HANDSHAKE_START) {
-//          if (token != NULL) {
-//            unsigned int curr_throttle_read = atoi(token + 1);
-//            if (curr_throttle_read >= 1000 and curr_throttle_read <= 2000) {
-//              bluetooth_throttle_read = curr_throttle_read;
-//            } 
-//          }
-//          token = strtok(NULL, ",");
-//          if (token != NULL) {
-//            unsigned int curr_steering_read = atoi(token);
-//            if (curr_steering_read >= 1000 and curr_steering_read <= 2000) {
-//              bluetooth_steering_read = curr_steering_read;
-//            }
-//          }
-//        } 
-//    }
-//
-//    void onRead(BLECharacteristic *characteristic)
-//    {
-//        // TODO revise what's being sent here
-//        characteristic->setValue("(1500,1500)");
-//        
-//    }
-//};
-//
-//void startBLE() {
-//  // Setup BLE Server
-//    BLEDevice::init(DEVICE_NAME);
-//    
-//    BLEServer *server = BLEDevice::createServer();
-//    server->setCallbacks(new MyServerCallbacks());
-//
-//    // Register message service that can receive messages and reply with a static message.
-//    BLEService *service = server->createService(SERVICE_UUID);
-//    controlBLECharacteristicMessage = service->createCharacteristic(CONTROL_UUID, 
-//                                                                    BLECharacteristic::PROPERTY_READ | 
-//                                                                    BLECharacteristic::PROPERTY_NOTIFY |
-//                                                                    BLECharacteristic::PROPERTY_WRITE);
-//    controlBLECharacteristicMessage->setCallbacks(new ControlCallback());
-////    controlBLECharacteristicMessage->addDescriptor(new BLE2902());
-//    service->start();
-//
-//    // TODO add another BLE service for changing BLE Name
-//
-//    // TODO add another BLE service for connecting to Wifi (2Ghz)
-//    
-//
-//    // Advertise services
-//    BLEAdvertising *advertisement = BLEDevice::getAdvertising();
-//    advertisement->addServiceUUID(SERVICE_UUID);
-//    advertisement->setScanResponse(true);
-//    advertisement->setMinPreferred(0x06);  // functions that help with iPhone connections issue
-//    advertisement->setMinPreferred(0x12);
-//    BLEDevice::startAdvertising();
-//    Serial.println("BLE Ready");
-//}
 
+volatile bool isClientConnected = false;
+bool isFlashLightOn = false;
+bool isRedLEDOn = false;
 
 
 void setup()
 {
   Serial.begin(115200);
-  startCamera();
-  startWebserver();
-//  startBLE();
-//  startServo();
+  Serial.println();
+  pinMode(RED_LED_PIN, OUTPUT);
+//  ledcSetup(0, 5000, 8);
+//  ledcAttachPin(FLASH_LED_PIN, 0);
+//  ledcWrite(FLASH_LED_PIN, 0);
+
+
+  setupCamera();
+  setupWifi();
+  setupRoutes();
+//  setupServo();
 }
 
-//void startServo(){
-//  throttleServo.attach(16, 1000, 2000);
-//}
-void startCamera() {
-    {
-    using namespace esp32cam;
-    Config cfg;
-    cfg.setPins(pins::AiThinker);
-    cfg.setResolution(hiRes);
-    cfg.setBufferCount(2);
-    cfg.setJpeg(80);
 
-    bool ok = Camera.begin(cfg);
-    Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
-  }
-}
-void startWebserver(){
-  
-  WiFi.persistent(false);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
-  Serial.print("IP Addr: ");
-  Serial.println(WiFi.localIP());
-  
-  Serial.print("Available routes:");
-  Serial.println("  /cam.bmp");
-  Serial.println("  /cam-lo.jpg");
-  Serial.println("  /cam-hi.jpg");
-  Serial.println("  /cam.mjpeg");
-
-  server.on("/cam.bmp", handleBmp);
-  server.on("/cam-lo.jpg", handleJpgLo);
-  server.on("/cam-hi.jpg", handleJpgHi);
-  server.on("/cam.jpg", handleJpg);
-  server.on("/cam.mjpeg", handleMjpeg);
-
-
-
-  server.begin();
-}
 
 void loop()
 {
-  server.handleClient();  
+  server.handleClient();
+//  writeToServo(); 
+  
+}
+
+void writeToServo() {
+   steeringServo.writeMicroseconds(ws_steering_read);    // tell servo to go to position 'steering'
+   throttleServo.writeMicroseconds(ws_throttle_read);    // tell motor to drive with power = motorPower   
+}
+
+// setup functions
+
+void setupServo() {
+  throttleServo.setPeriodHertz(50);
+  throttleServo.attach(THROTTLE_PIN, 1000, 2000);
+  steeringServo.setPeriodHertz(50);    // standard 50 hz servo
+  steeringServo.attach(STEERING_PIN, 1000, 2000); // attaches the servo on pin (whatever you assign)
+}
+
+void setupRoutes() {
+  Serial.print("http://");
+  Serial.println(WiFi.localIP());
+  Serial.println("  /cam-lo.jpg");
+  Serial.println("  /cmd/<THROTTLE>,<STEERING>");
+
+  server.on("/cam-lo.jpg", handleJpgLo);
+  server.on(UriBraces("/cmd/{}"), handleCmd);
+ 
+  server.begin();
 }
 
 
+void setupWifi() {
+  WiFi.disconnect(true);
+
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.print("Connecting ...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    blinkRedLED();
+    Serial.print(".");
+  }
+  digitalWrite(RED_LED_PIN, LOW);
+  Serial.println("Connected!");
+}
+void setupCamera() {
+//  {
+//    using namespace esp32cam;
+//    Config cfg;
+//    cfg.setPins(pins::AiThinker);
+//    cfg.setResolution(loRes);
+//    cfg.setBufferCount(2);
+//    cfg.setJpeg(80);
+//
+//    bool ok = Camera.begin(cfg);
+//    Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
+//  }
 
 
-void handleBmp()
-{
-  if (!esp32cam::Camera.changeResolution(loRes)) {
-    Serial.println("SET-LO-RES FAIL");
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG; 
+  
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_VGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
   }
 
-  auto frame = esp32cam::capture();
-  if (frame == nullptr) {
-    Serial.println("CAPTURE FAIL");
-    server.send(503, "", "");
+  // Camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
     return;
+  } else {
+    Serial.println("Camera Init Success");
   }
-  Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
-                static_cast<int>(frame->size()));
-
-  if (!frame->toBmp()) {
-    Serial.println("CONVERT FAIL");
-    server.send(503, "", "");
-    return;
-  }
-  Serial.printf("CONVERT OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
-                static_cast<int>(frame->size()));
-
-  server.setContentLength(frame->size());
-  server.send(200, "image/bmp");
-  WiFiClient client = server.client();
-  frame->writeTo(client);
+  
 }
 
-void serveJpg()
-{
-  auto frame = esp32cam::capture();
-  if (frame == nullptr) {
-    Serial.println("CAPTURE FAIL");
-    server.send(503, "", "");
-    return;
+// handle routes
+void handleCmd() {
+  String argument = server.pathArg(0);
+  
+  char buf[argument.length()] = "\0";
+  argument.toCharArray(buf, argument.length());
+  char *token = strtok(buf, ",");
+  if (token != NULL) {
+    unsigned int curr_throttle_read = atoi(token+1);
+    if (curr_throttle_read >= 1000 and curr_throttle_read <= 2000) {
+      ws_throttle_read = curr_throttle_read;
+    } 
   }
-  Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
-                static_cast<int>(frame->size()));
-
-  server.setContentLength(frame->size());
-  server.send(200, "image/jpeg");
-  WiFiClient client = server.client();
-  frame->writeTo(client);
+  token = strtok(NULL, ",");
+  if (token != NULL) {
+    unsigned int curr_steering_read = atoi(token);
+    if (curr_steering_read >= 1000 and curr_steering_read <= 2000) {
+      ws_steering_read = curr_steering_read;
+    }
+  } 
+//  Serial.print(ws_throttle_read);
+//  Serial.print(" ");
+//  Serial.print(ws_steering_read);
+//  Serial.println();
+  server.send(200, "text/plain","ack");
 }
 
 void handleJpgLo()
@@ -251,37 +206,37 @@ void handleJpgLo()
   if (!esp32cam::Camera.changeResolution(loRes)) {
     Serial.println("SET-LO-RES FAIL");
   }
-  serveJpg();
-}
-
-void handleJpgHi()
-{
-  if (!esp32cam::Camera.changeResolution(hiRes)) {
-    Serial.println("SET-HI-RES FAIL");
-  }
-  serveJpg();
-}
-
-void handleJpg()
-{
-  server.sendHeader("Location", "/cam-hi.jpg");
-  server.send(302, "", "");
-}
-
-void handleMjpeg()
-{
-  if (!esp32cam::Camera.changeResolution(hiRes)) {
-    Serial.println("SET-HI-RES FAIL");
-  }
-
-  Serial.println("STREAM BEGIN");
-  WiFiClient client = server.client();
-  auto startTime = millis();
-  int res = esp32cam::Camera.streamMjpeg(client);
-  if (res <= 0) {
-    Serial.printf("STREAM ERROR %d\n", res);
+  auto frame = esp32cam::capture();
+  if (frame == nullptr) {
+    Serial.println("CAPTURE FAIL");
+    server.send(503, "", "");
     return;
   }
-  auto duration = millis() - startTime;
-  Serial.printf("STREAM END %dfrm %0.2ffps\n", res, 1000.0 * res / duration);
+
+  server.setContentLength(frame->size());
+  server.send(200, "image/jpeg");
+  WiFiClient client = server.client();
+  frame->writeTo(client);
+}
+
+
+// Utility functions
+void blinkFlashlight() {
+  if (isFlashLightOn) {
+    ledcWrite(FLASH_LED_PIN, 0);
+    isFlashLightOn = false;
+  } else {
+    ledcWrite(FLASH_LED_PIN, 100);
+    isFlashLightOn = true;
+  }
+}
+
+void blinkRedLED() {
+  if (isRedLEDOn) {
+    digitalWrite(RED_LED_PIN, HIGH);
+    isRedLEDOn = false;
+  } else {
+    digitalWrite(RED_LED_PIN, LOW);
+    isRedLEDOn = true;
+  }
 }
--- misc/esp_32.py
+++ misc/esp_32.py
@@ -6,9 +6,9 @@ import time
 
 
 def main(host):
-    ws = websocket.WebSocket()
-    ws.connect(f"ws://{host}:81/ws")
-
+    # ws = websocket.WebSocket()
+    # ws.connect(f"ws://{host}:81/ws")
+    start = time.time()
     while True:
         imgResp = urllib.request.urlopen(f'http://{host}/cam-lo.jpg')
         imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
@@ -20,10 +20,13 @@ def main(host):
         # res = input("Enter throttle, steering: ")
         res = "1800,1600"
         throttle, steering = res.split(",")
-        ws.send(f"({throttle},{steering})")
+        urllib.request.urlopen(f'http://{host}/cmd/({throttle},{steering})')
+
+        print(f"Frame rate = {1 / (time.time() - start)}")
+        start = time.time()
         time.sleep(0.025)
 
 
 if __name__ == "__main__":
-    host = "10.0.0.8"
+    host = "10.0.0.14"
     main(host=host)
